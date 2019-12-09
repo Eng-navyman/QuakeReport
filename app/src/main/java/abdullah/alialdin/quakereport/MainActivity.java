5adf2cbef3e6d2008087fbbfab9d7b0223e7c7ac
Abdullah Alialdin <eng.navyman@gmail.com>
2019-12-09 21:11:21 +0200

    AsynkTask

--- app/src/main/java/abdullah/alialdin/quakereport/MainActivity.java
+++ app/src/main/java/abdullah/alialdin/quakereport/MainActivity.java
@@ -2,7 +2,9 @@ package abdullah.alialdin.quakereport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,32 +14,65 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=20";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Create a fake list of earthquake locations.
-        final ArrayList<EarthQuake> earthquakes = QueryUtils.extractEarthQuake();
 
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
+    }
+
+    private void updateUi(final ArrayList<EarthQuake> earthQuakes) {
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
         EarthQuakeAdapter adapter = new EarthQuakeAdapter(
-                this, earthquakes);
+                this, new ArrayList<EarthQuake>());
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                EarthQuake earthQuake = earthquakes.get(i);
+                EarthQuake earthQuake = earthQuakes.get(i);
                 Intent intent = new Intent(Intent.ACTION_VIEW, earthQuake.getUrl());
-                if (intent.resolveActivity(getPackageManager()) != null){
+                if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
                 }
             }
         });
+        adapter.clear();
+        if (earthQuakes != null && !earthQuakes.isEmpty()) {
+            adapter.addAll(earthQuakes);
+        }
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<EarthQuake>> {
+
+        @Override
+        protected ArrayList<EarthQuake> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            return QueryUtils.fetchEarthQuakeData(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<EarthQuake> result) {
+            if (result == null) {
+                return;
+            }
+            updateUi(result);
+        }
     }
 }
